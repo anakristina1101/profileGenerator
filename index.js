d785f35ea226a0b7775d94d607b5bcbd53c9134c
Ana Smith <anakristina1101@yahoo.com>
2019-11-17 16:32:20 -0600

    index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const inquirer = require('inquirer')
+var pdf = require('pdfmake')
+const axios = require('axios');
+
+inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "Please provide your github username?",
+            name: "userName"
+        },
+        {
+            type: "input",
+            message: "What's your favorite color?",
+            name: "colorChoice"
+        }
+
+    ])
+
+    .then(answers => {
+        queryURL = "https://api.github.com/users/" + userName;
+
+        axios
+        .get(queryURL)
+        .then(function(response) {
+            console.log(response);
+
+
+            fs.writeFile("profile.html", queryURL, (err) => {
+                if (err) {
+                    return console.log(err);
+                } else {
+                    const doc = new pdf;
+
+
+                
